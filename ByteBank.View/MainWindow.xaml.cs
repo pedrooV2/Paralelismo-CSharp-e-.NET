9cc94e43b8c2c51a605f5c041a21e0ca6711c141
Pedro <pedro.lucas210600@gmail.com>
2020-11-25 15:33:41 -0300

    Correct project

--- ByteBank.View/MainWindow.xaml.cs
+++ ByteBank.View/MainWindow.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using ByteBank.Core.Model;
 using ByteBank.Core.Repository;
 using ByteBank.Core.Service;
-using ByteBank.View.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,7 +23,6 @@ namespace ByteBank.View
     {
         private readonly ContaClienteRepository r_Repositorio;
         private readonly ContaClienteService r_Servico;
-        private CancellationTokenSource _cts;
 
         public MainWindow()
         {
@@ -34,80 +32,77 @@ namespace ByteBank.View
             r_Servico = new ContaClienteService();
         }
 
-        private async void BtnProcessar_Click(object sender, RoutedEventArgs e)
+        private void BtnProcessar_Click(object sender, RoutedEventArgs e)
         {
-            BtnProcessar.IsEnabled = false;
-
-            _cts = new CancellationTokenSource();
-
             var contas = r_Repositorio.GetContaClientes();
 
-            PgsProgresso.Maximum = contas.Count();
+            var contasQuantidadePorThread = contas.Count() / 4;
 
-            LimparView();
+            var contas_parte1 = contas.Take(contasQuantidadePorThread);
+            var contas_parte2 = contas.Skip(contasQuantidadePorThread).Take(contasQuantidadePorThread);
+            var contas_parte3 = contas.Skip(contasQuantidadePorThread*2).Take(contasQuantidadePorThread);
+            var contas_parte4 = contas.Skip(contasQuantidadePorThread*3);
+
+            var resultado = new List<string>();
+
+            AtualizarView(new List<string>(), TimeSpan.Zero);
 
             var inicio = DateTime.Now;
 
-            BtnCancelar.IsEnabled = true;
-            var progress = new Progress<String>(str =>
-                PgsProgresso.Value++);
-            //var byteBankProgress = new ByteBankProgress<String>(str =>
-            //  PgsProgresso.Value++);
-
-            try
+            Thread thread_parte1 = new Thread(() =>
             {
-                var resultado = await ConsolidarContas(contas, progress, _cts.Token);
-
-                var fim = DateTime.Now;
-                AtualizarView(resultado, fim - inicio);
-            }
-            catch (OperationCanceledException)
-            {
-                TxtTempo.Text = "Operação cancelada pelo usuário";
-            } 
-            finally
-            {
-                BtnProcessar.IsEnabled = true;
-                BtnCancelar.IsEnabled = false;
-            }
-        }
-
-        private void BtnCancelar_Click(object sender, RoutedEventArgs e)
-        {
-            BtnCancelar.IsEnabled = false;
-            _cts.Cancel();
-        }
-
-        private async Task<string[]> ConsolidarContas(IEnumerable<ContaCliente> contas, IProgress<string> reportadorDeProgresso, CancellationToken ct)
-        {
-            var tasks = contas.Select(conta =>
-                Task.Factory.StartNew(() =>
+                foreach (var conta in contas_parte1)
                 {
-                    ct.ThrowIfCancellationRequested();
+                    var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
+                    resultado.Add(resultadoProcessamento);
+                }
+            });
+            Thread thread_parte2 = new Thread(() =>
+            {
+                foreach (var conta in contas_parte2)
+                {
+                    var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
+                    resultado.Add(resultadoProcessamento);
+                }
+            });
+            Thread thread_parte3 = new Thread(() =>
+            {
+                foreach (var conta in contas_parte3)
+                {
+                    var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
+                    resultado.Add(resultadoProcessamento);
+                }
+            });
+            Thread thread_parte4 = new Thread(() =>
+            {
+                foreach (var conta in contas_parte4)
+                {
+                    var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
+                    resultado.Add(resultadoProcessamento);
+                }
+            });
 
-                    var resultadoConsolidacao = r_Servico.ConsolidarMovimentacao(conta, ct);
+            thread_parte1.Start();
+            thread_parte2.Start();
+            thread_parte3.Start();
+            thread_parte4.Start();
 
-                    reportadorDeProgresso.Report(resultadoConsolidacao);
+            while (thread_parte1.IsAlive || thread_parte2.IsAlive
+                || thread_parte3.IsAlive || thread_parte4.IsAlive )
+            {
+                Thread.Sleep(250);
+                //Não vou fazer nada
+            }
+            
+            var fim = DateTime.Now;
 
-                    ct.ThrowIfCancellationRequested();
-                    return resultadoConsolidacao;
-                }, ct)
-            );
-
-            return await Task.WhenAll(tasks);
+            AtualizarView(resultado, fim - inicio);
         }
 
-        private void LimparView()
-        {
-            LstResultados.ItemsSource = null;
-            TxtTempo.Text = null;
-            PgsProgresso.Value = 0;
-        }
-
-        private void AtualizarView(IEnumerable<String> result, TimeSpan elapsedTime)
+        private void AtualizarView(List<String> result, TimeSpan elapsedTime)
         {
             var tempoDecorrido = $"{ elapsedTime.Seconds }.{ elapsedTime.Milliseconds} segundos!";
-            var mensagem = $"Processamento de {result.Count()} clientes em {tempoDecorrido}";
+            var mensagem = $"Processamento de {result.Count} clientes em {tempoDecorrido}";
 
             LstResultados.ItemsSource = result;
             TxtTempo.Text = mensagem;
